f20f81c8f9b9886446d117acdfff361cbbcdce9b
Roger Marvin <20122077@kaishi-pu.ac.jp>
2024-12-12 11:32:28 +0900

    commit

--- app.py
+++ app.py
@@ -9,19 +9,18 @@ import random
 # CORS(app, resources={r"/*": {"origins": "http://192.168.182.140:5000"}})
 # socketio = SocketIO(app)
 
-app = Flask(__name__, static_folder='dist', static_url_path='')
+app = Flask(__name__, static_folder='./dist/assets', template_folder='./dist')
 CORS(app)
 socketio = SocketIO(app, cors_allowed_origins="*")
-
 
 #Socket to Index.html
 @app.route('/')
 def index():
-    return send_from_directory(app.static_folder, 'index.html')
+    return send_from_directory(app.template_folder, 'index.html')
 
 @app.errorhandler(404)
 def not_found(e):
-    return send_from_directory('dist', '404.html')
+    return send_from_directory(app.template_folder, '404.html')
 
 @app.route('/<path:path>')
 def static_proxy(path):
@@ -124,6 +123,5 @@ def handle_spin_result(data):
 
 #Run Server
 if __name__ == '__main__':
-    print("Server is running on port 5000")
-    print("================================")
-    socketio.run(app, host='0.0.0.0', port=5000,allow_unsafe_werkzeug=True)  
+    port = int(os.environ.get('PORT', 5000))
+    socketio.run(app, host='0.0.0.0', port=port)
