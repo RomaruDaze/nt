c2de694d6b93d281819a30d4063d1e52f1d19cf2
Roger Marvin <20122077@kaishi-pu.ac.jp>
2024-12-12 12:16:00 +0900

    commit

--- app.py
+++ app.py
@@ -23,8 +23,8 @@ import random
 #     return send_from_directory(app.static_folder, path)
 
 app = Flask(__name__, 
-            static_folder='build/assets',  # For serving static assets
-            template_folder='build')       # For serving index.html
+            static_folder='./build/assets',  # For serving static assets
+            template_folder='./build')       # For serving index.html
 CORS(app)
 socketio = SocketIO(app, cors_allowed_origins="*")
 
@@ -33,7 +33,7 @@ socketio = SocketIO(app, cors_allowed_origins="*")
 def serve(path):
     try:
         if path.startswith('assets/'):
-            return send_from_directory('build', path)
+            return send_from_directory('./build', path)
         return send_from_directory(app.template_folder, 'index.html')
     except Exception as e:
         print(f"Error serving file: {e}")
